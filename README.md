5f0f8e4b48e32855426dfe139a54dbd8edc39ce1
John Bosco Kisomba <johnboscogt265@gmail.com>
2026-03-08 00:22:20 +0300

    Initial commit (EMS Terminal Version)

---
+++ README.md
@@ -0,0 +1,4 @@
+#######CHALLENGES & SOLUTIONS
+
+####Problem: "undefined"
+I ran into a problem of getting the output as "undefined" when trying to access the object members of employee (e.g. name, title)
---
+++ script.js
@@ -0,0 +1,237 @@
+// EMPLOYEE MANAGEMENT SYSTEM PROJECT (JAVASCRIPT)
+/*
+ ***Features
+ *Add Employee✔
+ *Remove Employee✔
+ *Edit/Update Employee Details✔
+ *Display Employee Details✔
+ *Display All Employee Details✔
+ *Display Department Employees
+ */
+
+const employee = {
+  // Object Variables
+  id: "Unregisterd",
+  name: "Employee",
+  title: "Regular",
+  department: "None",
+  gender: "Unknown",
+  age: "Unknown",
+  salary: "Hidden",
+  paymentPeriod: "Unknown",
+  employeeList: [],
+
+  // Add new Employee method
+  addEmployee(
+    employeeName,
+    employeeTitle,
+    employeeDepartment,
+    employeeGender,
+    employeeAge,
+    employeeSalary,
+    employeePaymentPeriod,
+    employeeid,
+  ) {
+    this.name = employeeName ?? "Employee";
+    this.title = employeeTitle ?? "Unknown";
+    this.department = employeeDepartment ?? "Unknown";
+    this.gender = employeeGender ?? "Unknown";
+    this.age = employeeAge ?? "Unknown";
+    this.salary = employeeSalary ?? "Unknown";
+    this.paymentPeriod = employeePaymentPeriod ?? "Unknown";
+    this.id = employeeid ?? "Unregistered";
+
+    // Push new employee data to the employeesList Array
+    this.employeeList.push({
+      id: this.id,
+      name: this.name,
+      title: this.title,
+      department: this.department,
+      gender: this.gender,
+      age: this.age,
+      salary: this.salary,
+      paymentPeriod: this.paymentPeriod,
+    });
+  },
+
+  // Show all employees method
+  showEmplyeesList() {
+    if (this.employeeList.length === 0) {
+      console.log("Employee database is empty");
+    } else {
+      for (const employee of this.employeeList) console.log(employee);
+    }
+  },
+
+  // Remove employee
+  removeEmployeeID(employeeID) {
+    // Re-assigning to a new array of values
+    this.employeeList = this.employeeList.filter(
+      (employee) => employee.id !== employeeID,
+    );
+  },
+
+  // Edit/Update Employee details
+  editEmployeeDetails(employeeID, target, value) {
+    // Filter employees to get the desired employee
+    const targetEmployee = this.employeeList.filter(
+      (employee) => employee.id === employeeID,
+    );
+
+    // If Employee ID is not found
+    if (targetEmployee.length === 0) {
+      console.log("Employee does not exist...");
+
+      // If ID exists update data based on employee property
+    } else {
+      switch (target) {
+        case "name":
+          targetEmployee[0].name = value;
+          break;
+        case "title":
+          targetEmployee[0].title = value;
+          break;
+        case "department":
+          targetEmployee[0].department = value;
+          break;
+        case "gender":
+          targetEmployee[0].gender = value;
+          break;
+        case "age":
+          targetEmployee[0].age = value;
+          break;
+        case "salary":
+          targetEmployee[0].salary = value;
+          break;
+        case "pay":
+          targetEmployee[0].paymentPeriod = value;
+          break;
+      }
+    }
+
+    // Display updated version of the employee
+    this.employeeDetails(targetEmployee[0].id);
+  },
+
+  // Display All Employees
+  allEmployees() {
+    console.log(`ALL EMPLOYEES DETAILS`);
+    for (const employee of this.employeeList) {
+      console.log(`
+        ID: ${employee.id}
+
+        Name: ${employee.name}
+    
+        Title: ${employee.title}
+    
+        Department: ${employee.department}
+    
+        Gender: ${employee.gender}
+    
+        Age: ${employee.age}
+    
+        Salary: ${employee.salary}
+    
+        Pay: ${employee.paymentPeriod}`);
+      console.log(employee);
+    }
+  },
+
+  // Display Employee details
+  employeeDetails(employeeID) {
+    const targetEmployee = this.employeeList.filter(
+      (employee) => employee.id === employeeID,
+    );
+
+    console.log(`EMPLOYEE DETAILS ${employeeID}
+
+    ID: ${targetEmployee[0].id}
+
+    Name: ${targetEmployee[0].name}
+
+    Title: ${targetEmployee[0].title}
+
+    Department: ${targetEmployee[0].department}
+
+    Gender: ${targetEmployee[0].gender}
+
+    Age: ${targetEmployee[0].age}
+
+    Salary: ${targetEmployee[0].salary}
+
+    Pay: ${targetEmployee[0].paymentPeriod}`);
+    console.log(targetEmployee[0]);
+  },
+};
+
+// Adding new employees
+employee.addEmployee(
+  "James Kasule",
+  "Manager",
+  "Marketing",
+  "Male",
+  25,
+  "21M",
+  "monthly",
+);
+employee.addEmployee("Johnson Male", "Secretary", "Finance", "Male");
+employee.addEmployee("Jacob Bbosa");
+employee.addEmployee();
+
+// Displaying Employees
+employee.showEmplyeesList();
+
+// Remove Employee
+employee.removeEmployeeID("Unregistered");
+
+// New employees List
+console.log("New List");
+employee.showEmplyeesList();
+
+// Add new employees and show list
+employee.addEmployee(
+  "David Lubwama",
+  "Regular",
+  "Finance",
+  "Male",
+  24,
+  "15.5M",
+  "Monthly",
+  "FD001RGEMP26M",
+);
+employee.addEmployee(
+  "Charlse Makula",
+  "Regular",
+  "Marketing",
+  "Male",
+  22,
+  "11.5M",
+  "Monthly",
+  "MD001RGEMP26M",
+);
+employee.addEmployee(
+  "Naluze Jean",
+  "Secretary",
+  "Finance",
+  "Female",
+  26,
+  "25.5M",
+  "Monthly",
+  "FD001MCEMP26F",
+);
+employee.showEmplyeesList();
+
+// Removed employee by id
+employee.removeEmployeeID("FD001RGEMP26M");
+
+// New employee list
+console.log("New Employee List");
+employee.showEmplyeesList();
+
+// Display single employee details
+console.log("Single Employee details");
+
+employee.editEmployeeDetails("FD001MCEMP26F", "name", "Matildah Miles");
+employee.editEmployeeDetails("FD001MCEMP26F", "title", "Assistant CEO");
+
+employee.allEmployees();
